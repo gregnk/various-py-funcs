5af719ba0a477a03678f5f13a5a141fb36656669
Gregory Karastergios <gregory@gregnk.com>
2023-06-01 14:43:26 -0400

    Create gregnk-collection.py

---
+++ gregnk-collection.py
@@ -0,0 +1,56 @@
+'''
+Code by Gregory N. Karastergios (gregnk.com)
+
+Permission to use, copy, modify, and/or distribute this software for
+any purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL
+WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
+
+from datetime import datetime
+
+# Last Updated 2023-06-01
+def remove_end_newline(input_str):
+    if (input_str[-1:] == "\n"):
+        return input_str[:-1]
+    else:
+        return input_str
+
+# Last Updated 2023-06-01
+def remove_forbidden_dir_chars(dir_str):
+    forbidden_chars = []
+
+    if os.name == 'nt':
+        forbidden_chars = ['<', '>', ':', '\"', '/', '\\', '|', '?', '*']
+    elif os.name == 'posix':
+        forbidden_chars = ['/']
+    
+    for char in forbidden_chars:
+        dir_str = dir_str.replace(char, "–")
+        
+    return dir_str
+
+# Last Updated 2023-06-01
+def get_os_dir_slash():
+    if os.name == 'nt':
+        return "\\"
+        
+    elif os.name == 'posix':
+        return "/"
+        
+    elif os.name == 'java':
+        return "/"
+        
+    else:
+        raise Exception("Unsupported OS")
+        
+# Last Updated 2023-06-01
+# Requries from datetime import datetime
+def print_current_time():
+    print("Time: " + datetime.now().strftime("%Y-%m-%d @ %I:%M:%S %p"))      
